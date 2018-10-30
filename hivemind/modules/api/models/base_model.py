a8b222f9f0286c464a95ca0c6913bbe430b2d9f3
Davis Raymond Muro <davisraymondmuro@outlook.com>
2018-10-30 12:50:10 +0300

    Modify BaseModel Variable Names - Rename created_date to created - Rename modified_date to modified

--- hivemind/modules/api/models/base_model.py
+++ hivemind/modules/api/models/base_model.py
@@ -8,16 +8,14 @@ class BaseModel(models.Model):
     """
     This is the Base Model.
     """
-    created_date = models.DateTimeField(
+    created = models.DateTimeField(
         'Created',
         help_text=f'The date and time on which the object was created.',
-        auto_now_add=True
-    )
-    modified_date = models.DateTimeField(
+        auto_now_add=True)
+    modified = models.DateTimeField(
         'Modified',
         help_text='The date and time on which the object was last modified.',
-        auto_now=True
-    )
+        auto_now=True)
 
     # pylint: disable=too-few-public-methods
     class Meta:
